6f5b9041ab3bab36cd3645f6a326ab91c105fdaa
9211ali <aliismailrana@gmail.com>
2024-06-02 14:35:46 +0500

    create tweet with turbo stream & turbo frames

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ApplicationRecord
   has_many :tweets, dependent: :destroy
 
   validates :username, uniqueness: { case_insensitive: false }, allow_blank: true
+
+  before_save :set_name, if: -> { username.present? && name.blank? }
+
+  def set_name
+    name = username.humanize
+  end
 end
